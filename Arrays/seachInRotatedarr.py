3f05c83edc8f343fa6a100a36f02567139bc8af2
kushbhandari <kushbhandari54@gmail.com>
2022-01-07 20:18:05 +0530

    Added the file sortedRotatedArraySum

--- Arrays/seachInRotatedarr.py
+++ Arrays/seachInRotatedarr.py
@@ -20,28 +20,28 @@ def binarySearch(arr,key):
             lower=mid+1
     return -1    
 
-if __name__=='__main__':
-    arr=[30, 40, 50, 10, 20]
-    key=10
-    temp=-1
-    for i in range(1,len(arr)):
-        if arr[i-1]>arr[i]:
-            temp=i
-arr1=arr[:temp]
-arr2=arr[temp:]
+# if __name__=='__main__':
+#     arr=[30, 40, 50, 10, 20]
+#     key=10
+#     temp=-1
+#     for i in range(1,len(arr)):
+#         if arr[i-1]>arr[i]:
+#             temp=i
+# arr1=arr[:temp]
+# arr2=arr[temp:]
 
-a=binarySearch(arr1,key)
-b=binarySearch(arr2,key)
-if a==-1 and b==-1:
-    print(-1)
-elif a==-1:    
-    print(len(arr1)+b)
-elif b==-1:
-    print(a)
+# a=binarySearch(arr1,key)
+# b=binarySearch(arr2,key)
+# if a==-1 and b==-1:
+#     print(-1)
+# elif a==-1:    
+#     print(len(arr1)+b)
+# elif b==-1:
+#     print(a)
 
-#arr=rotateArr(arr,temp)
-#print(arr)
-#print(binarySearch(arr,key))
+# #arr=rotateArr(arr,temp)
+# #print(arr)
+# #print(binarySearch(arr,key))
 
 
 
